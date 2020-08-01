331f8b070192d1a958f97d4aedcfec2f10a85398
botao zhang <botao@botaos-iMac.local>
2020-08-01 14:02:18 +0800

    add new result

--- houseperiodanalyzer.py
+++ houseperiodanalyzer.py
@@ -7,11 +7,11 @@ import re
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 500)
 
-    data_from = pd.read_csv("./his/6_13/analyzed_result.csv")
-    data_to = pd.read_csv("./his/7_18/analyzed_result.csv")
+    data_from = pd.read_csv("./his/7_4/analyzed_result.csv")
+    data_to = pd.read_csv("./his/8_1/analyzed_result.csv")
 
-    data_to = data_to[["id","listprice"]]
-    data = data_from.merge(data_to,how="outer",on="id")
+    data_to = data_to[["id", "listprice"]]
+    data = data_from.merge(data_to, how="outer", on="id")
 
     data_sold = data.loc[data['listprice_y'].isna()]
     print("sold houses")
